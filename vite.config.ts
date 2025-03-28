080a1580098cdfa69f7e918cede439e6d1ec2c46
ltxlong <416803647@qq.com>
2025-03-28 15:44:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,6 @@ export default defineConfig({
   },
   server: {
     port: 5173,
-    host: true,
-    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:3001',
